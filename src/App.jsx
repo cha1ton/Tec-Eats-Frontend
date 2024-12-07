ad853fc7b94ec7a4cb42e5f4a03c5c845b0dd77c
cha1ton <chalton.mercado@tecsup.edu.pe>
2024-12-06 21:17:20 -0500

    Mostrar Navbar y Footer solo en rutas específicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-// src/components/user/App.jsx
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import HomePage from "./components/HomePage";
 import DishList from "./components/DishList";
-import Cart from "./components/Cart"; // Importamos el componente del carrito
-import Checkout from "./components/Checkout"; // Importamos el componente Checkout
-import UserProfile from './components/UserProfile';  // Importar el componente UserProfile
-
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import UserProfile from './components/UserProfile';
 import "./components/Styles.css";
 
 const AppUser = () => {
-  const [cart, setCart] = useState([]); // Estado para almacenar los elementos del carrito
+  const [cart, setCart] = useState([]); 
 
   const addToCart = (item) => {
     setCart((prevCart) => [...prevCart, item]);
@@ -23,37 +21,55 @@ const AppUser = () => {
     setCart((prevCart) => prevCart.filter((item) => item.id !== id));
   };
 
-  // Función para calcular el total de la compra
   const calculateTotal = () => {
     return cart.reduce((total, item) => total + item.price, 0);
   };
 
-  // Función que simula el proceso de pago
   const handlePayment = () => {
     alert("¡Pago realizado con éxito!");
-    setCart([]); // Vaciar el carrito después del pago
+    setCart([]);
   };
 
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<HomePage addToCart={addToCart} />} />
-        <Route path="/restaurant/:restaurantId" element={<DishList addToCart={addToCart} />} />
-        <Route
-          path="/cart"
-          element={<Cart cartItems={cart} removeItem={removeItemFromCart} />}
-        />
-        <Route path="/checkout" element={<Checkout cartItems={cart} removeItem={removeItemFromCart} calculateTotal={calculateTotal} handlePayment={handlePayment}
-        />
-        }
-        />{" "}
-        {/* Ruta para la página de pago */}
-        <Route path="/profile" element={<UserProfile />} /> {/* Ruta para el perfil de usuario */}
-      </Routes>
-      <Footer />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<HomePage addToCart={addToCart} />} />
+          <Route path="/restaurant/:restaurantId" element={<DishList addToCart={addToCart} />} />
+          <Route
+            path="/cart"
+            element={<Cart cartItems={cart} removeItem={removeItemFromCart} />}
+          />
+          <Route
+            path="/checkout"
+            element={
+              <Checkout
+                cartItems={cart}
+                removeItem={removeItemFromCart}
+                calculateTotal={calculateTotal}
+                handlePayment={handlePayment}
+              />
+            }
+          />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
+      </Layout>
     </Router>
+  );
+};
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  const showNavbarAndFooter = location.pathname !== "/";
+
+  return (
+    <>
+      {showNavbarAndFooter && <Navbar />}
+      {children}
+      {showNavbarAndFooter && <Footer />}
+    </>
   );
 };
 
